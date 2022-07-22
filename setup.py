de72ea90af8882e85dc6db93c9e834fe634e9688
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-22 17:08:09 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='aria2p',
+      version='0.1.1',
+      description='Python module for interacting with Chatbase APIs',
+      url='https://github.com/github-noob-arjun/aria2p',
+      author='Github',
+      author_email='None',
+      license='MIT Pro',
+      packages=['aria2p'],      
+      zip_safe=False)
